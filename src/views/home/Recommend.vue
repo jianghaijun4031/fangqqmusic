4ae53ab1f25e9e4ffc2cedd86c1baea2be7ee724
haijun <jianghaijun4031@126.com>
2019-06-15 11:12:45 +0800

    小东西

--- src/views/home/Recommend.vue
+++ src/views/home/Recommend.vue
@@ -1,12 +1,43 @@
 <template>
     <div>
-        我是推荐的内容
+        我推荐
     </div>
 </template>
 
 <script>
     export default {
-        name: "Recommend"
+        name: "Recommend",
+        data() {
+            return {
+                slider: [
+                    {
+                        "linkUrl": "https://y.qq.com/m/digitalbum/gold/index.html?openinqqmusic=1&_video=true&mid=004M8HLn4QOyyn&g_f=shoujijiaodian",
+                        "picUrl": "http://y.gtimg.cn/music/common/upload/MUSIC_FOCUS/1379062.jpg",
+                        "id": 21564
+                    },
+                    {
+                        "linkUrl": "http://y.qq.com/m/act/sfhd/267.html?ADTAG=jdt&openinqqmusic=1",
+                        "picUrl": "http://y.gtimg.cn/music/common/upload/MUSIC_FOCUS/1382153.jpg",
+                        "id": 21553
+                    },
+                    {
+                        "linkUrl": "https://y.qq.com/m/digitalbum/gold/index.html?openinqqmusic=1&_video=true&mid=001plzoF2wWJiA&g_f=shoujijiaodian",
+                        "picUrl": "http://y.gtimg.cn/music/common/upload/MUSIC_FOCUS/1378600.jpg",
+                        "id": 21561
+                    },
+                    {
+                        "linkUrl": "https://y.qq.com/m/digitalbum/gold/index.html?openinqqmusic=1&_video=true&mid=0010AYuF3nqxhw&g_f=shoujijiaodian",
+                        "picUrl": "http://y.gtimg.cn/music/common/upload/MUSIC_FOCUS/1375466.jpg",
+                        "id": 21508
+                    },
+                    {
+                        "linkUrl": "https://y.qq.com/m/digitalbum/gold/index.html?openinqqmusic=1&_video=true&mid=002aGyPo2OOc4c&g_f=shoujijiaodian",
+                        "picUrl": "http://y.gtimg.cn/music/common/upload/MUSIC_FOCUS/1378870.jpg",
+                        "id": 21567
+                    }
+                ]
+            }
+        }
     }
 </script>
 
